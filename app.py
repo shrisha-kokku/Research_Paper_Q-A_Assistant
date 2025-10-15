89c0dff1ab8cbfa040502d4b7608fa0fe01a6dee
Shrisha Kokku <124485644+shrisha-kokku@users.noreply.github.com>
2025-10-15 14:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.document_loaders import PyPDFLoader
 
 import os
