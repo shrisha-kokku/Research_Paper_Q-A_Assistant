68c04f6a22f39d17cf374444af2891988b90a053
Shrisha Kokku <124485644+shrisha-kokku@users.noreply.github.com>
2025-10-19 11:12:24 +0530

    Fix import path for create_stuff_documents_chain

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
